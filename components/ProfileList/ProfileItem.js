f4464eb72cca2233d423c33b603faa4c76a37415
shaikhaalammmari <shaikhaala@live.com>
2020-08-25 21:13:49 +0300

    Setup done all ready

---
+++ components/ProfileList/ProfileItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { observer } from "mobx-react";
+
+const ProfileItem = () => {};
+
+export default observer(ProfileItem);
---
+++ components/TripList/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { observer } from "mobx-react";
+import { List } from "native-base";
+
+const TripList = () => {
+  const tripList = tripStore.trips.map((trip) => (
+    <TripItem trip={trip} key={trip.id} />
+  ));
+  return (
+    <Content>
+      <List>{tripList}</List>
+    </Content>
+  );
+};
+
+export default observer(TripList);
