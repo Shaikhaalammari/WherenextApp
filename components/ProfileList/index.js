53ba2d38060f1779f62e0f8c0542d82906c54270
TheMshary <themshary94@gmail.com>
2020-08-26 21:07:08 +0300

    Code Reviewed

--- components/ProfileList/index.js
+++ components/ProfileList/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { observer } from "mobx-react";
 
+// don't create this file if youre not currently working on the profile features
+// keep the master branch clean all the time.
+
 const ProfileList = () => {};
 
 export default observer(ProfileList);
--- components/TripList/TripItem.js
+++ components/TripList/TripItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { observer } from "mobx-react";
 
+// clearly the trip list feature doesnt work.
+
 const TripItem = () => {};
 
 export default observer(TripItem);
