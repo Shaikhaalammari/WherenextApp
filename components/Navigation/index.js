30f2fe4ad3b6dde1deafff86d6d1fa154abb2bbb
shaikhaalammmari <shaikhaala@live.com>
2020-08-29 13:38:23 +0300

    Signup working ,need to uncomment and check logout

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -4,16 +4,29 @@ import { createStackNavigator } from "@react-navigation/stack";
 //Components
 import Home from "../Home";
 import TripList from "../TripList";
+import Signin from "../../authentication/Signin";
+import Signup from "../../authentication/Signup";
 
 const { Navigator, Screen } = createStackNavigator();
 const RootNavigator = () => {
   return (
-    <Navigator initialRouteName="Home">
-      <Screen name="Home" component={Home} />
+    <Navigator initialRouteName="Signup">
+      <Screen name="Home" component={Home} options={{ headerShown: false }} />
       <Screen
         name="Trips"
         component={TripList}
         options={{ title: "Choose a Trip" }}
+      />
+      <Screen
+        name="Signin"
+        component={Signin}
+        options={{ headerShown: false }}
+      />
+
+      <Screen
+        name="Signup"
+        component={Signup}
+        options={{ headerShown: false }}
       />
     </Navigator>
   );
---
+++ stores/authStore.js
@@ -0,0 +1,61 @@
+import { decorate, observable } from "mobx";
+import instance from "./instance";
+import decode from "jwt-decode";
+
+class AuthStore {
+  user = null;
+
+  setUser = (token) => {
+    // localStorage.setItem("myToken", token);
+    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+    this.user = decode(token);
+  };
+
+  signup = async (userData) => {
+    try {
+      const res = await instance.post("/signup", userData);
+      this.setUser(res.data.token);
+      console.log("AuthStore -> signin -> res.data.token", res.data);
+    } catch (error) {
+      console.log("AuthStore -> signup -> error", error);
+    }
+  };
+
+  signin = async (userData) => {
+    try {
+      const res = await instance.post("/signin", userData);
+      this.setUser(res.data.token);
+
+      console.log("authStore -> signin -> res.data", res.data);
+    } catch (error) {
+      console.log("AuthStore -> signin -> error", error);
+    }
+  };
+  //   signout = () => {
+  //     delete instance.defaults.headers.common.Authorization;
+  //     // localStorage.removeItem("myToken");
+  //     this.user = null;
+  //   };
+  //   checkForToken = () => {
+  //     // const token = localStorage.getItem("myToken");
+  //     console.log("hiii", token);
+  //     if (token) {
+  //       const currentTime = Date.now() / 1000;
+  //       const user = decode(token); // it was jwt_decode bs i made it decode to work!
+  //       if (user.expires >= currentTime) {
+  //         this.setUser(token);
+  //       } else {
+  //         this.signout();
+  //       }
+  //     }
+  //   };
+}
+
+decorate(AuthStore, {
+  user: observable,
+});
+
+const authStore = new AuthStore();
+// authStore.checkForToken();
+
+export default authStore;
