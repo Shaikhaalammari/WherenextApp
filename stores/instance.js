6399249173c857fd18c5fdfc92b7e1e9f197cbe9
alia-alabduljalil <aliaanmar@gmail.com>
2020-09-01 19:34:49 +0300

    A few edits

--- stores/instance.js
+++ stores/instance.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({
-  // baseURL: "http://192.168.0.155:8000",
-  baseURL: "http://localhost:8000",
-});
+const instance = axios.create({});
 
 export default instance;
