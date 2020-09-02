397d309b8f000974a9f46c4145300d36c13d1d87
FN Otaibi <nsayemlil@hotmail.com>
2020-09-02 19:27:16 +0300

    created descover list

---
+++ components/DiscoverList/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components/native";
+import { Icon } from "native-base";
+
+export const DiscoverContainer = styled.View`
+  flex: 1;
+  align-self: stretch;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  padding-right: 60px;
+  padding-left: 60px;
+`;
+
+export const DiscoverTitle = styled.Text`
+  color: #838383;
+  font-size: 24px;
+  margin-bottom: 20px;
+  border-bottom-color: #91d18b;
+`;
+
+export const DiscoverTextInput = styled.TextInput`
+  align-self: stretch;
+  text-align: left;
+  height: 40px;
+  margin-bottom: 30px;
+  color: #3b6978;
+  border-bottom-color: #3b6978;
+  border-bottom-width: 1px;
+`;
+
+export const AuthOther = styled.Text`
+  color: #99b898;
+  margin-top: 15px;
+`;
+
+export const DiscoverItemStyled = styled.Text`
+  color: #000000;
+  font-size: 18;
+  margin-top: 20;
+  margin-bottom: 10;
+  margin-left: 10;
+`;
+
+export const DiscoverTextStyled = styled.Text`
+  text-align: center;
+  color: #99b898;
+`;
+
+export const TrashIcon = styled(Icon)`
+  color: #e5e5e5;
+`;
